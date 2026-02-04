5713b05e0f07fe20f4dc0c7a3afb7ae4e806cf1b
Edward <edwardallan.au@gmail.com>
2026-02-04 15:27:59 +1100

    Initial commit

---
+++ src/test/java/com/exam/grocery_shop/GroceryShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.grocery_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
